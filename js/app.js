5ebd59fd1b18c007184415803a61d71c76842c58
Billy Buehl <wbuehl@ndc.nasa.gov>
2021-02-23 16:55:29 -0500

    app.js added/ minor style updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+
+function nextPage() {
+    console.log("next page!");
+}
+
+function backPage() {
+    console.log("back page!");
+}
+
+
 $(document).ready(function() {
     console.log("loaded!");
+
+    $("#next").click(nextPage);
+    $("#back").click(backPage);
 });
